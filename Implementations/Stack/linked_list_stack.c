64de28dc8da9d7685478d232956401563357c37d
Sathvik PN <66002135+SathvikPN@users.noreply.github.com>
2022-03-01 18:11:52 +0530

    Create linked_list_stack.c

---
+++ Implementations/Stack/linked_list_stack.c
@@ -0,0 +1,45 @@
+/* Stack Implementation using linked list (dynamic memory allocation) */
+#include<stdio.h>
+#include<stdlib.h>
+
+struct s_listnode
+{
+    int element;
+    struct s_listnode *pnext;
+};
+
+struct s_listnode *stack_buffer = NULL; // start empty
+
+void push(int elem)
+{
+    struct s_listnode *new_node = (struct s_listnode*)malloc(sizeof(struct s_listnode));
+    new_node->element = elem;
+    new_node->pnext = stack_buffer;
+    stack_buffer = new_node;
+}
+
+int pop()
+{
+    if(stack_buffer)
+    {
+        struct s_listnode *pelem = stack_buffer;
+        int elem = stack_buffer->element;
+        stack_buffer = pelem->pnext;
+        free(pelem); // remove node from the memory
+        return elem;
+    }
+    else
+    {
+        return 0; /* or other special value */
+    }
+}
+
+int main()
+{
+    push(1);
+    push(2);
+    push(3);
+    printf("%d ", pop());
+    printf("%d ", pop());
+    return 0;
+}
