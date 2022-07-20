e630956bbde1dac002a30c6a941d6e5fb372bd7b
Sathvik PN <sathvikapn@gmail.com>
2022-07-21 01:03:09 +0530

    Update llist.c

--- implementations/llist.c
+++ implementations/llist.c
@@ -15,11 +15,13 @@ struct node* addfront(int data, struct node** head);
 struct node* addback(int data, struct node** head); /* unmodified head. for pattern only*/
 
 struct node* find(int data, struct node* head);
+struct node* delnode(struct node* pelement, struct node** head); /* head might change */
+void freelist(struct node** head);
 
 
 int main()
 {
-    int val=5;
+    int val=2;
     struct node* address;
     addfront(2, &head);
     addfront(3, &head);
@@ -27,6 +29,10 @@ int main()
     addback(5, &head);
     display(head);
     address = find(val, head);
+    delnode(address, &head);
+    display(head);
+    freelist(&head);
+    display(head);
     return 0;
 }
 
@@ -72,12 +78,13 @@ struct node* addback(int data, struct node** head_address)
 void display(struct node* head)
 {
     /* head pointer passed by value ==> local copy */
+    printf("List: ");
     while(head)
     {
         printf("[%d] --> ", head->data);
         head = head->next;
     }
-    puts("[NULL]\n");
+    puts("[NULL]");
     return;
 }
 
@@ -95,4 +102,53 @@ struct node* find(int data, struct node* head)
     }
     printf("[%d] NOT FOUND\n", data);
     return NULL;
+}
+
+struct node* delnode(struct node* pelement, struct node** head)
+{
+    if(*head==NULL)
+    {
+        puts("Delete operation on empty list");
+        return *head;
+    }
+
+    if(pelement==NULL)
+    {
+        puts("No element passed to delete");
+        return *head;
+    }
+
+    if (pelement==*head)
+        *head = (*head)->next; /* update head pointer */
+    else
+    {
+        struct node* pnode = *head;
+        while((pnode!=NULL) && (pnode->next!=pelement))
+            pnode= pnode->next;
+        if(pnode==NULL)
+        {
+            puts("Element pointer not found");
+            exit(1);
+        }
+        pnode->next = pnode->next->next;
+    }
+    
+    printf("[%d] deleted\n", pelement->data);
+    free(pelement);
+    return *head;
+}
+
+void freelist(struct node** head)
+{
+    printf("Deleting nodes: ");
+    struct node* dnode;
+    while(*head)
+    {
+        dnode = *head;
+        *head = (*head)->next;
+        printf("[%d] ", dnode->data);
+        free(dnode);
+    }
+    printf("(Done)\n");
+    free(*head);
 }
