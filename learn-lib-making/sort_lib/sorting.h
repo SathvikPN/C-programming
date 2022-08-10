605ca33af48a502e441212e8a356e9f059a1f239
Sathvik PN <sathvikapn@gmail.com>
2022-08-10 22:14:48 +0530

    Delete learn-lib-making directory

--- learn-lib-making/sort_lib/sorting.h
+++
@@ -1,9 +0,0 @@
-#ifndef SORTING_H
-#define SORTING_H
-
-void show_array(int *array, int length);
-void swap(int *x, int *y);
-void array_copy(int* from_array, int* to_array, int length);
-int comparator_func(const void *a, const void *b);
-int compare_arrays(int*arrayA, int lenA, int*arrayB, int lenB);
-#endif
